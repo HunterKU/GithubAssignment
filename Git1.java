39e3198deff2817867ec3353908c2620720d964b
Hunter Kelley <hunterkelleyelec@gmail.com>
2021-01-31 22:01:32 -0600

    First commit

---
+++ Git1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class Github{
+  public static void main(String[] args){
+    Scanner s1 = new Scanner(System.in);
+    System.out.println("Enter your name");
+    String a  = s1.nextLine();
+    System.out.println("Hello " + a);
+  }
+}
